80a4e963b6b5dec7ad1dbf413ef78f01506d4ec9
Pablo Medina <113461293+ppablomedina@users.noreply.github.com>
2025-12-01 10:38:56 +0000

    Update utils.py

--- nextbike/utils.py
+++ nextbike/utils.py
@@ -1,4 +1,6 @@
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
